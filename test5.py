010206bb4ea5c499bf31d13337b34b27587e1f77
ansh ankul <57483078+ansh-ankul@users.noreply.github.com>
2022-06-07 12:50:41 +0530

    initial commit
    First and final

---
+++ test5.py
@@ -0,0 +1,8 @@
+def add(a,b):
+    return a+b
+
+def mult(a,b):
+    return a*b
+
+def power(a,b):
+    return a**b
---
+++ test6.py
@@ -0,0 +1,11 @@
+data = {
+    "name":"ansh",
+    "course":["ML", "DL", "py", "stats", "cv" ],
+    "message":"This is ansh's class"
+}
+
+def get_course():
+    return data["course"]
+
+def msg():
+    return data["message"]
